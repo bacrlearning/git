19b534619e3a18fbfb9d10d0d97870c3bfca8426
bac <=>
2019-05-27 18:02:36 +0700

    first commit

---
+++ hello.R
@@ -0,0 +1,15 @@
+#27/5 firt commit to github
+# Thu ham nano filename xem nao (21/5)
+# Them hello ngay 21/5
+
+
+#library
+library(dplyr)
+library(ggplot2)
+
+#import data
+mpg
+mtcars
+iris
+Vocab
+Diamond
